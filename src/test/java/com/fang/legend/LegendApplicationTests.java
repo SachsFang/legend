dde3ba0274d1de57ccc7aa2fdd3ffda3f8481d28
Sachs Fang <sachs.fang@cynovan.com>
2021-05-19 19:41:22 +0800

    init project

---
+++ src/test/java/com/fang/legend/LegendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fang.legend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LegendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
